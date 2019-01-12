b0c8606faa7373b7992574ad161837e67f6d5f87
taka yone <tkhtynd1991@gmail.com>
2019-01-12 11:27:49 +0900

    Initial Commit

---
+++ PodCastLBTA/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PodCastLBTA
+//
+//  Created by takahitoyoneda on 2019/01/12.
+//  Copyright © 2019 takahitoyoneda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
